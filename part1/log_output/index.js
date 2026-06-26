c9f45f5d02185ef363287a31dbb85ade0565b883
MarineAnimal <annam.finland@gmail.com>
2026-06-26 13:44:36 +0300

    Update index.js
    forgot to build index.js

--- part1/log_output/index.js
+++ part1/log_output/index.js
@@ -1,1 +1,10 @@
+const { v4: uuidv4 } = require("uuid");
 
+const id = uuidv4();
+
+console.log("Log output started");
+
+setInterval(() => {
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp}: ${id}`);
+}, 5000);
