0724380d49315e1205474242f4113f60d5d9aa21
Anna Makarevich <annam.finland@gmail.com>
2026-06-29 13:42:36 +0300

    Add exercise 1.4 (todo app deployment)

---
+++ 1.4/src/index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.end("Todo app running\n");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server started in port ${PORT}`);
+});
