5cb48c9f483d268b2e81a7f8287c273d879bdfd0
MarineAnimal <annam.finland@gmail.com>
2026-06-29 16:18:18 +0300

    left an easter egg in index.js

--- 1.2/src/index.js
+++ 1.2/src/index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.end("Todo app running\n");
+  res.end("My todo app is running. cool! in case anyone sees this, the weather today is terrible :'D.\n");
 });
 
 server.listen(PORT, () => {
