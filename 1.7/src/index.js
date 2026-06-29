eb205077af13cc8249fac19027aa5ae856048694
MarineAnimal <annam.finland@gmail.com>
2026-06-29 16:35:56 +0300

    added app

--- 1.7/src/index.js
+++ 1.7/src/index.js
@@ -2,15 +2,22 @@ const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const greeting = process.env.DEMO_GREETING;
-const farewell = process.env.DEMO_FAREWELL;
+
+// generate random string ONCE on startup
+const randomString = Math.random().toString(36).substring(2, 15);
+
+setInterval(() => {
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp}: ${randomString}`);
+}, 5000);
 
 app.get("/", (req, res) => {
+  const timestamp = new Date().toISOString();
   res.send(`
     <html>
       <body>
-        <h1>${greeting}</h1>
-        <p>${farewell}</p>
+        <p>${timestamp}</p>
+        <p>${randomString}</p>
       </body>
     </html>
   `);
